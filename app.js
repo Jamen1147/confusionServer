366a6afe39a82f3d16b221d8580665d987527e06
Jamen1147 <jamen1147@gmail.com>
2020-01-17 10:26:29 +1100

    authentication

--- app.js
+++ app.js
@@ -46,37 +46,22 @@ app.use(
 	})
 );
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
-	console.log(req.signedCookies);
+	console.log(req.session);
 
 	if (!req.session.user) {
-		const authHeader = req.headers.authorization;
-
-		if (!authHeader) {
-			const err = new Error('You are not authenticated');
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
-
-		const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-		const [ username, password ] = auth;
-
-		if (username === 'admin' && password === 'password') {
-			req.session.user = 'admin';
-			next();
-		} else {
-			const err = new Error('You are not authenticated');
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
+		var err = new Error('You are not authenticated!');
+		err.status = 403;
+		return next(err);
 	} else {
-		if (req.session.user === 'admin') {
+		if (req.session.user === 'authenticated') {
 			next();
 		} else {
-			const err = new Error('You are not authenticated');
-			err.status = 401;
+			var err = new Error('You are not authenticated!');
+			err.status = 403;
 			return next(err);
 		}
 	}
@@ -86,8 +71,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishesRouter);
 app.use('/promotions', promosRouter);
 app.use('/leaders', leadersRouter);
